2e4ee6fd0db95e01866f90d5f0826dde9be3645c
Amit Kumar Rai <96979864+Amit2001kumar@users.noreply.github.com>
2025-09-03 22:39:58 +0530

    test

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, eam-collaboration-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, team-collab');
   });
 });
